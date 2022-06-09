4715366bbcbad1e2342a6476f8781e5a1dcbf45c
hwcer <hwclegend@163.com>
2022-06-09 13:37:30 +0800

    develop

--- values/values.go
+++ values/values.go
@@ -21,23 +21,27 @@ func (m Values) Set(key string, val interface{}) interface{} {
 	return val
 }
 
-func (m Values) Add(key string, val int64) (r int64) {
-	r = m.GetInt(key) + val
-	m[key] = r
-	return
+//func (m Values) Add(key string, val int64) (r int64) {
+//	r = m.GetInt64(key) + val
+//	m[key] = r
+//	return
+//}
+//
+//func (m Values) Sub(key string, val int64) (r int64) {
+//	r = m.GetInt64(key) - val
+//	m[key] = r
+//	return
+//}
+
+func (m Values) GetInt(key string) int {
+	return int(m.GetInt64(key))
 }
 
-func (m Values) Sub(key string, val int64) (r int64) {
-	r = m.GetInt(key) - val
-	m[key] = r
-	return
+func (m Values) GetInt32(key string) int32 {
+	return int32(m.GetInt64(key))
 }
 
-func (m Values) GetInt32(key string) int32{
-	return int32(m.GetInt(key))
-}
-
-func (m Values) GetInt(key string) int64 {
+func (m Values) GetInt64(key string) int64 {
 	v, ok := m[key]
 	if !ok {
 		return 0
@@ -49,6 +53,8 @@ func (m Values) GetInt(key string) int64 {
 		return int64(v.(int32))
 	case int64:
 		return v.(int64)
+	case float32:
+		return int64(v.(float32))
 	case float64:
 		return int64(v.(float64))
 	case string:
@@ -59,25 +65,24 @@ func (m Values) GetInt(key string) int64 {
 	}
 }
 
+func (m Values) GetFloat32(key string) float32 {
+	return float32(m.GetInt64(key))
+}
 
-func (m Values) GetFloat(key string) (r float64) {
+func (m Values) GetFloat64(key string) (r float64) {
 	v, ok := m[key]
 	if !ok {
 		return 0
 	}
 	switch v.(type) {
-	case int:
-		r = float64(v.(int))
-	case int32:
-		r = float64(v.(int32))
-	case int64:
-		r = float64(v.(int64))
 	case float32:
 		r = float64(v.(float32))
 	case float64:
 		r = v.(float64)
 	case string:
 		r, _ = strconv.ParseFloat(v.(string), 10)
+	default:
+		return float64(m.GetInt64(key))
 	}
 	return
 }
