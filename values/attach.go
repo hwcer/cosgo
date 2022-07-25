fb6fe7a25f36fee610956f546d2701ebc1254793
hwcer <hwclegend@163.com>
2022-07-25 18:15:27 +0800

    develop

---
+++ values/attach.go
@@ -0,0 +1,34 @@
+package values
+
+import "encoding/json"
+
+type Attach string
+
+var attachNoneBytes = []byte("\"\"")
+
+func (a *Attach) UnmarshalJSON(b2 []byte) error {
+	*a = Attach(b2)
+	return nil
+}
+
+func (a *Attach) MarshalJSON() ([]byte, error) {
+	if a == nil || len(*a) == 0 {
+		return attachNoneBytes, nil
+	}
+	r := []byte(*a)
+	return r, nil
+}
+
+//Marshal 将一个对象放入Attach
+func (a *Attach) Marshal(v interface{}) error {
+	d, err := json.Marshal(v)
+	if err == nil {
+		*a = Attach(d)
+	}
+	return err
+}
+
+//Unmarshal 使用i解析Attach
+func (a *Attach) Unmarshal(i interface{}) error {
+	return json.Unmarshal([]byte(*a), i)
+}
