e9c2fdb1a149811ba82b4f3b2ab154585d8d9311
万超 黄 <hwclegend@163.com>
2024-05-10 18:53:07 +0800

    清理

--- funcs.go
+++ funcs.go
@@ -4,19 +4,6 @@ import (
 	"path/filepath"
 )
 
-//func GO(fn func()) {
-//	SCC.GO(fn)
-//}
-//
-//func CGO(fn func(ctx context.Context)) {
-//	SCC.CGO(fn)
-//}
-//
-//// SGO 带崩溃保护
-//func SGO(fn func(context.Context), handles ...utils.TryHandle) {
-//	SCC.SGO(fn, handles...)
-//}
-
 // Abs 获取以工作路径为起点的绝对路径
 func Abs(dir ...string) string {
 	path := filepath.Join(dir...)
@@ -38,9 +25,4 @@ func Dir() string {
 // Name 项目内部获取appName
 func Name() string {
 	return Config.GetString(AppName)
-}
-
-// WorkDir 程序工作目录 废弃
-func WorkDir() string {
-	return Config.GetString(AppDir)
 }
