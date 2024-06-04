3813860151c9e8f66b3073351aa5f938e0465d8d
万超 黄 <hwclegend@163.com>
2024-06-04 14:06:04 +0800

    移除冗余代码

--- random/random.go
+++ random/random.go
@@ -4,38 +4,48 @@ import (
 	"sort"
 )
 
+type Data struct {
+	Key int32 //序号
+	Val int32 //权重
+}
+
+func (this *Data) GetKey() int32 {
+	return this.Key
+}
+func (this *Data) GetVal() int32 {
+	return this.Val
+}
+
+type Less func(a, b *Data) bool
+
 type Random struct {
-	items [][3]int32 //[id,Roll,Weight]
+	items []*Data //[id,Roll]
 	total int32
 }
 
-func New(items map[int32]int32) *Random {
+func New(items map[int32]int32, less ...Less) *Random {
 	r := &Random{}
 	for k, v := range items {
-		r.items = append(r.items, [3]int32{k, v, 0})
+		r.items = append(r.items, &Data{Key: k, Val: v})
 		r.total += v
 	}
-	r.init()
+	var f Less
+	if len(less) > 0 {
+		f = less[0]
+	} else {
+		f = r.less
+	}
+	r.Sort(f)
 	return r
 }
+func (this *Random) less(i, j *Data) bool {
+	return i.Val > j.Val
+}
 
-func (this *Random) init() {
+func (this *Random) Sort(f Less) {
 	sort.Slice(this.items, func(i, j int) bool {
-		return this.items[i][1] > this.items[j][1]
+		return f(this.items[i], this.items[j])
 	})
-	x := this.total
-	var no int32
-	for i, v := range this.items {
-		p := v[1]
-		if no > 0 {
-			p = p * x / no
-			no = no * (x - p) / x
-		} else {
-			no = x - p
-		}
-		v[2] = p
-		this.items[i] = v
-	}
 }
 
 // Roll 简单的权重算法，直接计算区间落点
@@ -44,57 +54,40 @@ func (this *Random) Roll() int32 {
 		return -1
 	}
 	rnd := Roll(1, this.total)
-	for _, v := range this.items {
-		if v[1] > 0 {
-			rnd -= v[1]
+	for _, d := range this.items {
+		if d.Val > 0 {
+			rnd -= d.Val
 			if rnd <= 0 {
-				return v[0]
+				return d.Key
 			}
 		}
 	}
 	return -1
 }
 
-// Weight 权重算法,每一条执行一次随机，如果不命中继续下一条
-// 按照权重从小到大执行, 最后一条(权重最大)作为保底
-// 执行结果和 Roll 基本一致
-// 优点是极限值出现的时机更加靠后,策划更加满意和放心
-func (this *Random) Weight() (r int32) {
-	if this.total == 0 {
-		return -1
-	}
-	for _, v := range this.items {
-		r = v[0]
-		if rnd := Roll(1, this.total); v[2] >= rnd {
-			return
-		}
-	}
-	return
-}
-
 // Multi 随机多个不重复
 func (this *Random) Multi(num int) (r []int32) {
 	if num >= len(this.items) {
-		for _, v := range this.items {
-			r = append(r, v[0])
+		for _, d := range this.items {
+			r = append(r, d.Key)
 		}
 		return
 	}
-	items := make([][3]int32, len(this.items))
+	items := make([]*Data, len(this.items))
 	limit := this.total
 	copy(items, this.items)
 
 	for i := 0; i < num; i++ {
 		rnd := Roll(1, limit)
-		for j, v := range items {
-			if v[1] <= 0 {
+		for j, d := range items {
+			if d.Val <= 0 {
 				continue
 			}
-			rnd -= v[1]
+			rnd -= d.Val
 			if rnd <= 0 {
-				r = append(r, v[0])
-				limit -= v[1]
-				items[j] = [3]int32{v[0], 0, 0}
+				r = append(r, d.Key)
+				limit -= d.Val
+				items[j] = &Data{Key: d.Key, Val: 0}
 				break
 			}
 		}
@@ -103,8 +96,8 @@ func (this *Random) Multi(num int) (r []int32) {
 }
 
 func (this *Random) Range(f func(k, v int32) bool) {
-	for _, v := range this.items {
-		if !f(v[0], v[1]) {
+	for _, d := range this.items {
+		if !f(d.Key, d.Val) {
 			return
 		}
 	}
