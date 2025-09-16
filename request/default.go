86d5ce064bcc651b9e76f68a2ad8adb88b34d85a
万超 黄 <hwclegend@163.com>
2025-09-16 19:26:49 +0800

    update

--- request/default.go
+++ request/default.go
@@ -10,6 +10,6 @@ func Post(url string, data interface{}, reply interface{}) (err error) {
 	return defaultRequest.Post(url, data, reply)
 }
 
-func Request(method, url string, data interface{}) (reply []byte, err error) {
-	return defaultRequest.Request(method, url, data)
+func Request(method, url string, data interface{}, headers ...map[string]string) (reply []byte, err error) {
+	return defaultRequest.Request(method, url, data, headers...)
 }
